775358ed8b3c46d2622905839700cfa9a5477d2c
Adrian0910 <uchihasantander@gmail.com>
2021-01-11 20:52:51 -0600

    Fin

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,11 +1,25 @@
-import { nombreCliente, ahorro } from './cliente.js';
+import nuevaFuncion, { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from './cliente.js';
+import { Empresa } from './empresa.js'
+
+
+nuevaFuncion();
+
 
 
 console.log(nombreCliente);
 console.log(ahorro);
+console.log(mostrarInformacion(nombreCliente, ahorro));
+
+tieneSaldo(ahorro);
+
+const cliente = new Cliente(nombreCliente, ahorro);
+
+console.log(cliente.mostrarInformacion());
 
 
-
+// Importar empresa
+const empresa = new Empresa('Developer Software', 500, 'Desarrollo de software');
+console.log(empresa.mostrarInformacion());
 
 
 
--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -3,4 +3,41 @@
 export const nombreCliente = 'Juan';
 export const ahorro = 20000;
 
+export function mostrarInformacion(nombre, ahorro) {
+    return `Cliente: ${nombre} - Ahorro: ${ahorro}`;
+}
+
+
+export function tieneSaldo(ahorro){
+    if(ahorro > 0) {
+        console.log('Si tienes saldo')
+    }else {
+        console.log('El cliente no tiene saldo')
+    }
+}
+
+
+export class Cliente {
+    constructor(nombre, ahorro){
+        this.nombre = nombre;
+        this.ahorro = ahorro;
+    }
+
+    mostrarInformacion(){
+        return `Cliente: ${this.nombre} - Ahorro: ${this.ahorro}`;
+    }
+
+}
+
+
+export default function nuevaFuncion(){
+    console.log('Este es el expor default');
+}
+
+
+
+
+
+
+
 
